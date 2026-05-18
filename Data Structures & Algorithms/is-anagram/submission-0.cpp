254599974ab126d5d5d2b0ba905af84a6a1e923b
shashank <23071a6745@vnrvjiet.in>
2026-05-18 12:40:17 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+        vector<int> hash(26, 0);
+        for(int i = 0; i < s.length(); i++) {
+            hash[s[i] - 'a']++;
+            hash[t[i] - 'a']--;
+        }
+        for(int count : hash) {
+            if(count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
